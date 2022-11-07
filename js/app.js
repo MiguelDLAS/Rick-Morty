7d8deda66371d445db439b958793f128f64c0eef
MIGUEL <mikiserrano2018@gmail.com>
2022-11-07 12:03:54 -0600

    carpeta ui.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { modalBody, spinner, creaCard} from './ui.js';
+
 const urlBase= 'https://rickandmortyapi.com/api/character/';
 
 const loadData=(urlBase, page=1)=>{
@@ -45,23 +47,6 @@ const showCharacters=(personajes)=> {
         
     });
 }
-const creaCard=(personaje)=>{
-    const html=`
-    <div class="card bg-dark text-ligth border" style="width: 18rem;">
-  <img src="${personaje.image}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${personaje.name}</h5>
-    <p class="card-text">${personaje.status}</p>
-    <a href="#" class="btn btn-primary"
-      data-bs-toggle="modal" 
-    data-bs-target="#exampleModal"
-    data-id="${personaje.id}">Ver mas</a>
-    
-  </div>
-</div>
-`;
-return html;
-}
 const navegacion=(e)=>{
 
     
@@ -70,17 +55,7 @@ const navegacion=(e)=>{
         loadData(urlBase, id);
     }
 }
-const modalBody=(personaje)=>{
-    const div=document.createElement('div');
-    div.classList.add('text-center');
-    let html=``;
-    html +=`<img src="${personaje.image}" class="thumbnail">`;
-    html +=`<p>${personaje.status}-${personaje.species}</p>`;
-    html +=`<p>Ultima ubicacion conocida</p><p>${personaje.origin.name}</p>`;
-    html +=`<p>Ha aparecido en ${personaje.episode.length} Episodios</p>`;
-    div.innerHTML=html;
-    return div;
-}
+
 
 const showCharacterById=(id)=>{
     const urlId=`${urlBase}${id}`; 
@@ -110,17 +85,7 @@ const loadInfo=(e)=>{
     }
 }
 
-const spinner=()=>{
-    const div=document.createElement('div');
-    const html=
-    `<div class="d-flex justify-content-center">
-    <div class="spinner-border text-light" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  </div>`;
-  div.innerHTML= html;
-  return div;
-}
+
 document.querySelector('#botones').addEventListener('click', navegacion);
 document.querySelector('#characters').addEventListener('click', loadInfo);
 
